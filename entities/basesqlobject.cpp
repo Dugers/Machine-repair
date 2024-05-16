423eab3b5220d6b9a01387cbff24ee67b5af730a
root <root@DESKTOP-0R2JH6C>
2024-05-16 18:11:01 +0700

    removed templates from SqlObject and moved implementation to .cpp

--- entities/basesqlobject.cpp
+++ entities/basesqlobject.cpp
@@ -8,5 +8,26 @@
 ////    return mId;
 ////}
 
-wrapper_is_empty::wrapper_is_empty(const char* Message) : std::runtime_error{Message} {}
-wrapper_is_empty::wrapper_is_empty(const std::string& Message) : std::runtime_error{Message} {}
+//wrapper_is_empty::wrapper_is_empty(const char* Message) : std::runtime_error{Message} {}
+//wrapper_is_empty::wrapper_is_empty(const std::string& Message) : std::runtime_error{Message} {}
+
+SqlObject::SqlObject(const int& id) :
+    mId{id}
+{}
+
+SqlObject::SqlObject(int&& id) :
+    mId{std::move(id)}
+{}
+
+
+void SqlObject::set_id(const int& id) {
+    mId = id;
+}
+
+void SqlObject::set_id(int&& id) {
+    mId = std::move(id);
+}
+
+const int& SqlObject::id() const {
+    return mId;
+}
--- entities/basesqlobject.h
+++ entities/basesqlobject.h
@@ -14,7 +14,7 @@
 //    const int& id() const;
 //};
 
-template <typename T>
+
 class SqlObject {
 private:
     int mId;
@@ -26,32 +26,10 @@ public:
     const int& id() const;
 };
 
-struct wrapper_is_empty : public std::runtime_error {
-public:
-    wrapper_is_empty(const char* Message);
-    wrapper_is_empty(const std::string& Message);
-};
-
-template <typename T>
-SqlObject<T>::SqlObject(const int& id) :
-    mId{id}
-{}
-template <typename T>
-SqlObject<T>::SqlObject(int&& id) :
-    mId{std::move(id)}
-{}
-
-template <typename T>
-void SqlObject<T>::set_id(const int& id) {
-    mId = id;
-}
-template <typename T>
-void SqlObject<T>::set_id(int&& id) {
-    mId = std::move(id);
-}
-template <typename T>
-const int& SqlObject<T>::id() const {
-    return mId;
-}
+//struct wrapper_is_empty : public std::runtime_error {
+//public:
+//    wrapper_is_empty(const char* Message);
+//    wrapper_is_empty(const std::string& Message);
+//};
 
 #endif // BASESQLOBJECT_H
