60fc3d08ca219894da0da31fc6aaffe69489d00b
root <root@DESKTOP-0R2JH6C>
2024-05-24 16:14:33 +0700

    deleted macros for enable and disable button

--- src/tools/widgets.h
+++ src/tools/widgets.h
@@ -3,14 +3,6 @@
 #include <QTableWidget>
 #include <QVector>
 
-#ifndef DISABLE_BUTTON
-    #define DISABLE_BUTTON(button) button->setEnabled(false);
-#endif
-
-#ifndef ENABLE_BUTTON
-    #define ENABLE_BUTTON(button) button->setEnabled(true);
-#endif
-
 void fill_table(QTableWidget* table, const QVector<QVector<QString>>& table_values);
 
 #endif // WIDGETS_H
