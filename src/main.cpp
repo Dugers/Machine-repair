fa8596ba7f79db80305e339de2e7c223e0ee387a
root <root@DESKTOP-0R2JH6C>
2024-05-28 12:50:12 +0700

    The application starts even if it fails to connect to the database

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include "authwindow.h"
 #include "models/db.h"
-
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    db::create_tables();
+    try {db::create_tables();} catch(...) {}
     AuthWindow w;
     w.show();
     return a.exec();
